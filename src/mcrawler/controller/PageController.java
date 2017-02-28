a9e4b454222947e0b5c8b312985ce88916ad9965
Jonathan Corral <johnlejardinnier@gmail.com>
2017-02-28 17:13:47 +0100

    fix(page): use treeview instead of listview

--- src/mcrawler/controller/PageController.java
+++ src/mcrawler/controller/PageController.java
@@ -2,28 +2,26 @@ package mcrawler.controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import javafx.beans.binding.Binding;
 import javafx.beans.binding.Bindings;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import mcrawler.MainApp;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
-import static mcrawler.lib.Tools.delete;
-import static mcrawler.lib.Tools.deleteDir;
-import static mcrawler.lib.Tools.file_exists;
+import static mcrawler.lib.Tools.*;
 
 /**
  * Created by jonathan on 10/02/17.
@@ -33,6 +31,10 @@ public class PageController {
     private MainApp mainApp;
 
     private JFXListView<Label> list = new JFXListView<Label>();
+
+    private TreeView<String> tree = new TreeView<>();
+
+    private String path;
 
     @FXML
     Label title;
@@ -62,18 +64,46 @@ public class PageController {
 
         scrollPane.fitToWidthProperty();
 
-        btnDelete.disableProperty().bind(Bindings.isEmpty(list.getSelectionModel().getSelectedItems()));
-        btnView.disableProperty().bind(Bindings.isEmpty(list.getSelectionModel().getSelectedItems()));
+        btnDelete.disableProperty().bind(Bindings.isEmpty(tree.getSelectionModel().getSelectedItems()));
+        //btnView.disableProperty().bind(Bindings.isEmpty(tree.getSelectionModel().getSelectedItems()));
+        btnView.setDisable(true);
+
+
+        tree.getSelectionModel().selectedItemProperty().addListener(new javafx.beans.value.ChangeListener<TreeItem<String>>() {
+            @Override
+            public void changed(ObservableValue<? extends TreeItem<String>> observable, TreeItem<String> oldValue, TreeItem<String> newValue) {
+
+                String filename = mainApp.getRootPath() + getSelectedPath();
+                File file = new File(filename);
+
+                //System.out.println("listen: " + filename);
+
+
+                if(file.isDirectory()) {
+                    btnView.setDisable(true);
+                }
+                else {
+                    btnView.setDisable(false);
+                }
+            }
+        });
+
+
     }
 
 
+
+
+
     public void showDetails(String name) {
+
+        this.path = mainApp.getRootPath() + "/" + name;
 
         title.setText(name);
         title.getStyleClass().add("title");
 
 
-        String previewFile = mainApp.getRootPath() + "/" + name + "/" + "preview.png";
+        String previewFile = path + "/" + "preview.png";
 
         if(file_exists(previewFile)) {
             Image image = new Image("file:" + previewFile);
@@ -83,30 +113,57 @@ public class PageController {
             pageContainer.add(preview, 0, 0);
         }
 
-        list.setLayoutY(14);
-        list.setPrefHeight(300);
-        list.setPrefWidth(370);
+        tree.setLayoutY(14);
+        tree.setPrefHeight(300);
+        tree.setPrefWidth(370);
 
 
-        initItems(name);
+        File file = new File(path);
 
+        findFiles(file,null);
 
-
-        pageContainer.add(list, 0, 1);
+        pageContainer.add(tree, 0 , 1);
 
     }
 
 
-    private void initItems(String name) {
+    private void findFiles(File dir, TreeItem<String> parent) {
+        TreeItem<String> root = new TreeItem<String>(dir.getName());
+        root.setExpanded(true);
+        try {
+            File[] files = dir.listFiles();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    System.out.println("directory:" + file.getCanonicalPath());
+                    findFiles(file,root);
+                } else {
+                    System.out.println("     file:" + file.getCanonicalPath());
+                    root.getChildren().add(new TreeItem<String>(file.getName()));
+                }
 
-        File actual = new File(mainApp.getRootPath() + "/" + name);
-
-        for( File file : actual.listFiles()){
-            list.getItems().add(new Label(file.getName()));
+            }
+            if(parent==null){
+                tree.setRoot(root);
+            } else {
+                parent.getChildren().add(root);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 
+
+    private String getSelectedPath() {
+        StringBuilder pathBuilder = new StringBuilder();
+        for (TreeItem<String> item = tree.getSelectionModel().getSelectedItem(); item != null ; item = item.getParent()) {
+
+            pathBuilder.insert(0, item.getValue());
+            pathBuilder.insert(0, "/");
+        }
+        String path = pathBuilder.toString();
+
+        return path;
+    }
 
     @FXML
     public void btnDeleteClick() {
@@ -114,31 +171,46 @@ public class PageController {
 
 
         String file = "index.html";
+        String filename = null;
 
-        if( list.getSelectionModel().getSelectedItems().size() > 0){
-            file = list.getSelectionModel().getSelectedItem().getText();
+        if( tree.getSelectionModel().getSelectedItems().size() > 0){
+            file = getSelectedPath();
+
+            filename = mainApp.getRootPath() + file;
+            System.out.println("Selected : " + filename);
+            System.out.println("path : " + path);
+
         }
 
 
-        String filename =  mainApp.getRootPath() + "/" + title.getText() + "/" + file;
 
+        if (filename != null && file_exists(filename)) {
 
-        if (file_exists(filename)) {
+            if(filename.equals(path)) {
+                deletePageClick();
+            } else {
 
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Delete item");
-            alert.setHeaderText("Delete file : " + file);
-            alert.setContentText("Are you sure to delete this file ?");
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Delete item");
+                alert.setHeaderText("Delete file : " + file);
+                alert.setContentText("Are you sure to delete this file ?");
 
-            Optional<ButtonType> result = alert.showAndWait();
-            if (result.get() == ButtonType.OK){
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK){
 
-                if(delete(filename)) {
-                    list.getItems().clear();
-                    initItems(title.getText());
+                    File rootFile = new File(path);
+                    File dir = new File(filename);
+
+                    if(delete(filename) || deleteDir(dir)) {
+                        tree.setRoot(null);
+                        findFiles(rootFile, null);
+                        tree.refresh();
+                    }
+
                 }
 
             }
+
 
         }
 
@@ -152,8 +224,8 @@ public class PageController {
         String name = title.getText();
         String file = "index.html";
 
-        if( list.getSelectionModel().getSelectedItems().size() > 0){
-            file = list.getSelectionModel().getSelectedItem().getText();
+        if( tree.getSelectionModel().getSelectedItems().size() > 0){
+            file = tree.getSelectionModel().getSelectedItem().getValue();
         }
 
 
@@ -200,7 +272,7 @@ public class PageController {
 
         System.out.println("Open file: " + file);
 
-        String filepath =  mainApp.getRootPath() + "/" + name + "/" + file;
+        String filepath =  path + "/" + file;
 
         if(file_exists(filepath)) {
 
